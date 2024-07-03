431b04b3e530be93e588b96233ce496384bdeb91
dheerajuparkoti <dheeraj.uparkoti.17@gmail.com>
2024-07-03 21:49:46 +0545

    fourth commit

--- resources/views/includes/header_navbar.blade.php
+++ resources/views/includes/header_navbar.blade.php
@@ -4,17 +4,23 @@
     <nav>
         <button class="menu-toggle" aria-label="Toggle navigation">&#9776;</button>
         <ul class="nav-menu">
-            <li class="menu-item">
-                <a href="#homea" class="menu-link">Main Home</a>
-                <ul class="sub-menu">
+            <div class="left-menu">
+                <li class="menu-item">
+                    <a href="#homea" class="menu-link">DASHBOARD</a>
+                    {{-- <ul class="sub-menu">
                     <li><a href="#homea" class="sub-menu-link">Homea</a></li>
                     <li><a href="#homeb" class="sub-menu-link">Homeb</a></li>
                     <li><a href="#homec" class="sub-menu-link">Homec</a></li>
-                </ul>
-            </li>
-            <li class="menu-item"><a href="#about" class="menu-link">About</a></li>
-            <li class="menu-item"><a href="#services" class="menu-link">Services</a></li>
-            <li class="menu-item"><a href="#contact" class="menu-link">Contact</a></li>
+                </ul> --}}
+                </li>
+                <li class="menu-item"><a href="#EXAM" class="menu-link">EXAM</a></li>
+                <li class="menu-item"><a href="#LIBRARY" class="menu-link">LIBRARY</a></li>
+                <li class="menu-item"><a href="#HALL" class="menu-link">HALL</a></li>
+                <li class="menu-item"><a href="#HALL" class="menu-link">ABOUT US</a></li>
+            </div>
+            <div class="right-menu">
+                <li class="menu-item"><a href="#HALL" class="menu-link-my-account">MY ACCOUNT</a></li>
+            </div>
         </ul>
     </nav>
 </header>
