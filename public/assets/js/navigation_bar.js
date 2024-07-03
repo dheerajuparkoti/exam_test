4a5d9fe5493bb5558e530266c709b6ef362c985a
dheerajuparkoti <dheeraj.uparkoti.17@gmail.com>
2024-07-03 22:05:32 +0545

    5th commit

--- public/assets/js/navigation_bar.js
+++ public/assets/js/navigation_bar.js
@@ -1,20 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navMenu = document.querySelector(".nav-menu");
-    const menuItems = document.querySelectorAll(".menu-item > .menu-link");
+document.querySelector(".menu-toggle").addEventListener("click", function () {
+    document.querySelector(".nav-menu").classList.toggle("active");
+});
 
-    menuToggle.addEventListener("click", function () {
-        navMenu.classList.toggle("active");
-    });
-
-    menuItems.forEach((item) => {
-        item.addEventListener("click", function (e) {
-            const subMenu = this.nextElementSibling;
-            if (subMenu && subMenu.classList.contains("sub-menu")) {
-                e.preventDefault();
-                subMenu.classList.toggle("active");
-                this.classList.toggle("active");
-            }
+document
+    .querySelectorAll(".menu-link, .menu-link-my-account")
+    .forEach((link) => {
+        link.addEventListener("click", function () {
+            document
+                .querySelectorAll(".menu-link, .menu-link-my-account")
+                .forEach((link) => link.classList.remove("active"));
+            this.classList.add("active");
         });
     });
-});
