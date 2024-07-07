eeedc80a34842d06f1e9fd7a1cdda18db9371a2e
dheerajuparkoti <dheeraj.uparkoti.17@gmail.com>
2024-07-07 17:27:56 +0545

    for merging

--- resources/views/users/exam/form.blade.php
+++ resources/views/users/exam/form.blade.php
@@ -11,16 +11,21 @@
 
 <body>
     <header>
-        
+        <div class="top-header">
+            <span>Powered By: <strong>SoftMine Tech Pvt. Ltd.</strong></span>
+        </div>
+        <div class="bottom-header">
+            <span>Timer <strong>SoftMine Tech Pvt. Ltd.</strong></span>
+        </div>
     </header>
 
+    <aside>
+        <div class="left-aside">
+    </aside>
 
 
 
     <footer class="text-center py-2">&copy; 2024 MCQ for Engineers. All rights reserved.</footer>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
